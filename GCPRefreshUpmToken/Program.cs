b77168a52150df5b980f9a9d26b6b4f073fba072
Christian Helmich <kagekirin@gmail.com>
2024-03-13 01:04:00 +0900

    feature (GCPRefreshUpmToken): implement functional shim (feature/shim-implementation) [#3]
    - **chore: change project file to correct encoding (UTF8, no BOM)**
    - **chore: change source file to correct encoding (UTF8, no BOM)**
    - **build (GCPRefreshUpmToken): add dependency on System.CommandLine v2.0.0-beta4.22272.1**
    - **build (GCPRefreshUpmToken): add dependency on System.CommandLine.NamingConventionBinder v2.0.0-beta4.22272.1**
    - **build (GCPRefreshUpmToken): add dependency on Xunit v2.7.0**
    - **feature (GCPRefreshUpmToken): implement functional shim**
    
    - **chore: change project file to correct encoding (UTF8, no BOM)**
    - **chore: change source file to correct encoding (UTF8, no BOM)**
    - **build (GCPRefreshUpmToken): add dependency on System.CommandLine v2.0.0-beta4.22272.1**
    - **build (GCPRefreshUpmToken): add dependency on System.CommandLine.NamingConventionBinder v2.0.0-beta4.22272.1**
    - **build (GCPRefreshUpmToken): add dependency on Xunit v2.7.0**
    - **feature (GCPRefreshUpmToken): implement functional shim**

--- GCPRefreshUpmToken/Program.cs
+++ GCPRefreshUpmToken/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.CommandLine;
+using System.CommandLine.IO;
+using System.CommandLine.NamingConventionBinder;
+using System.CommandLine.Parsing;
+
+namespace GCPRefreshUpmToken;
+
+public class Program
+{
+    public static async Task<int> Main(string[] args)
+    {
+        var rootCommand = new RootCommand(
+            "Refreshes `.upmconfig.toml` with a new token from Google Cloud."
+        )
+        {
+            new Option<Uri>(
+                aliases: ["--registry", "-r"],
+                description: "URL of the Google Cloud Artifact Registry"
+            ),
+            new Option<FileInfo>(aliases: ["--config", "-c"], description: "config file"),
+        };
+        rootCommand.Handler = CommandHandler.Create(Run);
+
+        return await rootCommand.InvokeAsync(args);
+    }
+
+    static async Task<int> Run(
+        Uri registry,
+        FileInfo config,
+        IConsole console,
+        CancellationToken cancellationToken
+    )
+    {
+        console.WriteLine($"hello: {registry} {config}");
+        return 1;
+    }
+}
