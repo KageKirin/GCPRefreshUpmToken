9b3c9d5400a7610a5a76e596daf462d6b0a04089
Christian Helmich <kagekirin@gmail.com>
2024-03-13 10:16:14 +0900

    feature (GCPRefreshUpmToken): implement core functionality -- registry token retrieval and safe storage in UPM config file (feature/real-implementation) [#5]
    - **build (GCPRefreshUpmToken): add dependency on Tomlyn v0.17.0**
    - **refactor (GCPRefreshUpmToken): enable nullable**
    - **feature (GCPRefreshUpmToken): implement procedure to retrieve registry token from `gcloud auth print-access-token`**
    - **feature (GCPRefreshUpmToken): implement safely writing retrieved token into provided UPM config file**
    
    - **build (GCPRefreshUpmToken): add dependency on Tomlyn v0.17.0**
    - **refactor (GCPRefreshUpmToken): enable nullable**
    - **feature (GCPRefreshUpmToken): implement procedure to retrieve registry token from `gcloud auth print-access-token`**
    - **feature (GCPRefreshUpmToken): implement safely writing retrieved token into provided UPM config file**

--- GCPRefreshUpmToken/Program.cs
+++ GCPRefreshUpmToken/Program.cs
@@ -3,8 +3,15 @@ using System.CommandLine;
 using System.CommandLine.IO;
 using System.CommandLine.NamingConventionBinder;
 using System.CommandLine.Parsing;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using Tomlyn;
+using Tomlyn.Model;
 
 namespace GCPRefreshUpmToken;
+
+#nullable enable
 
 public class Program
 {
@@ -33,6 +40,115 @@ public class Program
     )
     {
         console.WriteLine($"hello: {registry} {config}");
-        return 1;
+
+        var error = TryGetRegistryToken(out var token);
+        console.WriteLine($"error: {error}\n{token}");
+
+        if (error != 0)
+            return error;
+
+        if (string.IsNullOrEmpty(token))
+            return -1;
+
+        var model = config.Exists
+            ? Toml.ToModel(await File.ReadAllTextAsync(config.FullName, cancellationToken))
+            : new TomlTable();
+        console.WriteLine($"before: {Toml.FromModel(model)}");
+
+
+
+        /// basically doing the following assignment: `model["npmAuth"][registry]["token"] = token`
+        if (!model.ContainsKey("npmAuth"))
+            model.Add("npmAuth", new TomlTable());
+        var npmAuthTable = (TomlTable)model["npmAuth"];
+
+        if (!npmAuthTable.ContainsKey(registry.ToString()))
+            npmAuthTable.Add(registry.ToString(), new TomlTable());
+        var registryTable = (TomlTable)npmAuthTable[registry.ToString()];
+
+        if (registryTable.ContainsKey("token"))
+            registryTable["token"] = token;
+        else
+            registryTable.Add("token", token);
+
+        console.WriteLine($"after: {Toml.FromModel(model)}");
+        await File.WriteAllTextAsync(
+            config.FullName,
+            Toml.FromModel(model),
+            Encoding.Default,
+            cancellationToken
+        );
+
+        return 0;
+    }
+
+    static int TryGetRegistryToken(out string token)
+    {
+        using var process = new System.Diagnostics.Process();
+        process.StartInfo.UseShellExecute = false;
+        process.StartInfo.CreateNoWindow = true;
+        process.StartInfo.RedirectStandardOutput = true;
+        process.StartInfo.RedirectStandardError = true;
+        process.StartInfo.FileName = @"gcloud";
+        process.StartInfo.WorkingDirectory = Environment.CurrentDirectory;
+        process.StartInfo.Arguments = @"auth print-access-token";
+
+        object stdoutBuilderLocker = new object();
+        object stderrBuilderLocker = new object();
+        StringBuilder stdoutBuilder = new();
+        StringBuilder stderrBuilder = new();
+        string errorLog = string.Empty;
+
+        process.OutputDataReceived += new DataReceivedEventHandler(
+            (object sender, DataReceivedEventArgs args) =>
+            {
+                lock (stdoutBuilderLocker)
+                {
+                    stdoutBuilder.Append(args.Data?.Trim() ?? string.Empty);
+                }
+            }
+        );
+
+        process.ErrorDataReceived += new DataReceivedEventHandler(
+            (object sender, DataReceivedEventArgs args) =>
+            {
+                var dataTrimmed = args.Data?.Trim() ?? string.Empty;
+                lock (stderrBuilderLocker)
+                {
+                    stderrBuilder.Append(args.Data?.Trim() ?? string.Empty);
+                }
+            }
+        );
+
+        process.Start();
+        process.BeginErrorReadLine();
+        process.BeginOutputReadLine();
+
+        var timeOut = 60;
+        if (process.WaitForExit(timeOut * 1000))
+        {
+            Console.WriteLine(
+                $"process {process.Id} has terminated with exit code {process.ExitCode}"
+            );
+        }
+        else
+        {
+            Console.Error.WriteLine(
+                $"process {process.Id} has timed out and will now be terminated"
+            );
+            process.Kill(entireProcessTree: true);
+            Console.Error.WriteLine(
+                $"process {process.Id} has been terminated with exit code {process.ExitCode}"
+            );
+        }
+
+        if (stdoutBuilder.Length > 0)
+            Console.WriteLine(stdoutBuilder.ToString());
+
+        if (stderrBuilder.Length > 0)
+            Console.Error.WriteLine(stderrBuilder.ToString());
+
+        token = stdoutBuilder.ToString();
+        return process.ExitCode;
     }
 }
